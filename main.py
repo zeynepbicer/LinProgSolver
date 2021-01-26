f3956916685c52ae55da4cfbd47b30e5a0861490
zeynep <mericzeynepbicer@gmail.com>
2021-01-26 18:58:56 +0000

    Added input file functionality!!!

--- main.py
+++ main.py
@@ -9,32 +9,61 @@ import numpy as np
 # constraint coefs - fills the const matrix
 # rhs array size = # of constraints
 # concatenate, then checks.
+f = open("inputs", 'r')
 
-nr_vars = int(input('number of decision variables: '))
-nr_const = int(input('number of constraints: '))
-obj = np.zeros(nr_vars+2*nr_const)
-const = np.zeros((nr_const, nr_vars))
+obj = []
+const = []
+rhs = []
+const_ineq = []
 
-for i in range(nr_vars):
-	obj[i] = input(f'obj fun coef {i+1}: ')
-print(obj)
-const_ineq = np.empty(nr_const, dtype=str)
+for idx, line in enumerate(f):
+	if idx == 0:
+		strs = line.split()
+		num_vars, num_const = int(strs[0]), int(strs[1])
+		obj = np.zeros(num_vars+2*num_const)
+		const = np.zeros((num_const, num_vars))
+		const_ineq = np.empty(num_const, dtype=str)
+		rhs = np.zeros(num_const)
+	elif idx == 1:
+		strs = line.split()
+		for i in range(num_vars):
+			obj[i] = float(strs[i])
+	else:
+		strs = line.split()
+		for col in range(num_vars):
+			const[idx-2,col] = float(strs[col])
+		const_ineq[idx-2] = strs[-2]
+		rhs[idx-2] = float(strs[-1])
 
-for row in range(nr_const):
-	for col in range(nr_vars):
-		const[row,col] = float(input(f'constraint {row+1}, coef {col+1}: '))
-	const_ineq[row] = input(' = , < , > : ')
 
-rhs = np.zeros(nr_const)
-for i in range(nr_const):
-	rhs[i] = int(input(f'rhs for const {i+1}: '))
 rhs = np.array([rhs])
+const_ineq = np.array(const_ineq)		
 
-slack = np.identity(nr_const)
-artif = np.zeros((nr_const,nr_const))
+# num_vars = int(input('number of decision variables: '))
+# num_const = int(input('number of constraints: '))
+# obj = np.zeros(num_vars+2*num_const)
+# const = np.zeros((num_const, num_vars))
+
+# for i in range(num_vars):
+# 	obj[i] = input(f'obj fun coef {i+1}: ')
+
+#const_ineq = np.empty(num_const, dtype=str)
+
+# for row in range(num_const):
+# 	for col in range(num_vars):
+# 		const[row,col] = float(input(f'constraint {row+1}, coef {col+1}: '))
+# 	const_ineq[row] = input(' = , < , > : ')
+
+# rhs = np.zeros(num_const)
+# for i in range(num_const):
+# 	rhs[i] = int(input(f'rhs for const {i+1}: '))
+# rhs = np.array([rhs])
+
+slack = np.identity(num_const)
+artif = np.zeros((num_const,num_const))
 
 big_matrix = np.concatenate((const, slack, artif, rhs.T), axis = 1)
-Cb = np.zeros(nr_const)
+Cb = np.zeros(num_const)
 num_rows, num_cols = big_matrix.shape
 cz = np.zeros(num_cols)
 z = np.zeros(num_cols)
@@ -42,29 +71,24 @@ i = 0
 M = 9999
 
 #checking for negative values on rhs
-for _rhs in rhs[0]:
+for index, _rhs in enumerate(rhs[0]):
 	if _rhs < 0:
-		index = int(np.where(rhs == _rhs)[0])
 		big_matrix[index, :] = -1*big_matrix[index, :]
 		if const_ineq[index] == '>':
 			const_ineq[index] = '<'
 		if const_ineq[index] == '<':
 			const_ineq[index] = '>'
 
-i = 0
-j = 0
-for val in const_ineq:
+
+for index, val in enumerate(const_ineq):
 	if val == '=':
-		index = int(np.where(const_ineq == val)[0][i])
-		big_matrix[index, nr_vars+row] == 0
-		i += 1
+		big_matrix[index, num_vars+row] == 0
 	elif val == '>':
-		index = int(np.where(const_ineq == val)[0][j])
-		big_matrix[index, -nr_const-1+index] = 1
-		big_matrix[index, nr_vars+index] = -1
-		obj[-nr_const+index] = -M
+		big_matrix[index, -num_const-1+index] = 1
+		big_matrix[index, num_vars+index] = -1
+		obj[-num_const+index] = -M
 		Cb[index] = -M
-		j += 1
+	
 iter = 0 
 
 for column in big_matrix.T:
@@ -81,9 +105,9 @@ while (True):
 	pivot_col = int(np.where(cz == max(cz))[0][0])
 	#var going in
 	
-	if pivot_col in range(nr_vars):
+	if pivot_col in range(num_vars):
 		var_in = 'x'
-	elif pivot_col in range(nr_vars, nr_vars+nr_const):
+	elif pivot_col in range(num_vars, num_vars+num_const):
 		var_in = 's'
 	else:
 		var_in = 'a'
@@ -104,8 +128,8 @@ while (True):
 	#gauss elim
 	pivot = big_matrix[pivot_row][pivot_col]
 	print(f'{var_in}_{pivot_col%3+1} going in {var_out}_{pivot_row%3+1} going out.')
-	_in.append(f'{var_in}_{pivot_col%3+1}')
-	out.append(f'{var_out}_{pivot_row%3+1}')
+	_in.append(f'{var_in}_{pivot_col%num_vars+1}')
+	out.append(f'{var_out}_{pivot_row%num_vars+1}')
 	Cb[pivot_row] = obj[pivot_col]
 	big_matrix[pivot_row, :] = big_matrix[pivot_row, :]/pivot
 
