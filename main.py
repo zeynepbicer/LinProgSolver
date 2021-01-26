c7cdf16dde2deb5197d476da206a550f0922f9c4
zeynep <mericzeynepbicer@gmail.com>
2021-01-26 15:58:16 +0000

    Fixed inequality checks

--- main.py
+++ main.py
@@ -51,17 +51,15 @@ for _rhs in rhs[0]:
 			const_ineq[index] == '<'
 		if const_ineq[index] == '<':
 			const_ineq[index] == '>'
-print(const_ineq)
-print(np.where(const_ineq == '=')[0])
 i = 0
 j = 0
 for val in const_ineq:
 	if val == '=':
-		index = int(np.where(const_ineq == val)[0,i])
+		index = int(np.where(const_ineq == val)[0][i])
 		big_matrix[index, nr_decision_vars+row] == 0
 		i+= 1
 	elif val == '>':
-		index = int(np.where(const_ineq == val)[0,j])
+		index = int(np.where(const_ineq == val)[0][j])
 		big_matrix[index, -nr_const-1+index] = 1
 		obj[-nr_const+index] = -M
 		Cb[index] = -M
@@ -69,42 +67,42 @@ for val in const_ineq:
 iter = 0 
 
 
-# while (True):
-#     #finding pivot element
-# 	pivot_col = int(np.where(cz == max(cz))[0][0])
-# 	temp = []
-# 	for i in range(num_rows):
-# 		if big_matrix[i][pivot_col] > 0:
-# 			temp.append(big_matrix[i][-1]/big_matrix[i][pivot_col])
-# 		else: 
-# 			temp.append(9999)
-# 	pivot_row = int(np.where(temp == min(temp))[0][0])
-# 	print(np.where(temp == min(temp)))
-# 	print(f'x_{pivot_col+1} entering the table, s_{pivot_row+1} exiting.')
-# 	#gauss elim
-# 	pivot = big_matrix[pivot_row][pivot_col]
+while (True):
+    #finding pivot element
+	pivot_col = int(np.where(cz == max(cz))[0][0])
+	temp = []
+	for i in range(num_rows):
+		if big_matrix[i][pivot_col] > 0:
+			temp.append(big_matrix[i][-1]/big_matrix[i][pivot_col])
+		else: 
+			temp.append(9999)
+	pivot_row = int(np.where(temp == min(temp))[0][0])
+	print(np.where(temp == min(temp)))
+	print(f'x_{pivot_col+1} entering the table, s_{pivot_row+1} exiting.')
+	#gauss elim
+	pivot = big_matrix[pivot_row][pivot_col]
 
-# 	Cb[pivot_row] = obj[pivot_col]
-# 	big_matrix[pivot_row, :] = big_matrix[pivot_row, :]/pivot
+	Cb[pivot_row] = obj[pivot_col]
+	big_matrix[pivot_row, :] = big_matrix[pivot_row, :]/pivot
 
-# 	for i in range(num_rows):
-# 		if i != pivot_row:
-# 			mult = big_matrix[i][pivot_col]
-# 			big_matrix[i, :] = big_matrix[i, :] - mult*big_matrix[pivot_row, :]
-# 	i = 0
+	for i in range(num_rows):
+		if i != pivot_row:
+			mult = big_matrix[i][pivot_col]
+			big_matrix[i, :] = big_matrix[i, :] - mult*big_matrix[pivot_row, :]
+	i = 0
 	
-# 	for column in big_matrix.T:
-# 		z[i] = np.dot(column, Cb)
-# 		i +=1
-# 	for i in range(len(obj)):
-# 		cz[i] = obj[i]-z[i]
-# 	print(max(cz))
+	for column in big_matrix.T:
+		z[i] = np.dot(column, Cb)
+		i +=1
+	for i in range(len(obj)):
+		cz[i] = obj[i]-z[i]
+	print(max(cz))
 
 	
-# 	if (all(i <= 0 for i in cz) or all(i !=0 for i in Cb)):
-# 		print(big_matrix)
-# 		print(cz)
-# 		print(f'finished in {iter+1} iterations. max value is = {z[-1]}')	
-# 		print(Cb)
-# 		break
-# 	iter += 1
+	if (all(i <= 0 for i in cz) or all(i !=0 for i in Cb)):
+		print(big_matrix)
+		print(cz)
+		print(f'finished in {iter+1} iterations. max value is = {z[-1]}')	
+		print(Cb)
+		break
+	iter += 1
