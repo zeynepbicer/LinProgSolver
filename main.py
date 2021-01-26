fb1e919b1d1945aa819852a49e4ad7f9ec937a0a
zeynep <mericzeynepbicer@gmail.com>
2021-01-26 17:27:32 +0000

    Verbose output

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import numpy as np
 
-
 # inputs:
 # number of decision variables
 # number of constraints
-# --- size of the obj function array =  #of d.v + #of const. x2
-# --- size of the const matrix = #of const, #of d.v
+# --- size of the obj function array =  # of d.v + # of const. x2
+# --- size of the const matrix = # of const, #of d.v
 # obj fun coefs - fills the obj
 # constraint coefs - fills the const matrix
 # rhs array size = # of constraints
@@ -25,7 +24,7 @@ for row in range(nr_const):
 	for col in range(nr_decision_vars):
 		const[row,col] = float(input(f'constraint {row+1}, coef {col+1}: '))
 	const_ineq[row] = input(' = , < , > : ')
-print(const)
+
 rhs = np.zeros(nr_const)
 for i in range(nr_const):
 	rhs[i] = int(input(f'rhs for const {i+1}: '))
@@ -74,10 +73,20 @@ for column in big_matrix.T:
 for i in range(len(obj)):
 	cz[i] = obj[i]-z[i]
 
-	
+#need to keep track of what variables are going in/out
+_in = []
+out = []
 while (True):
     #finding pivot element
 	pivot_col = int(np.where(cz == max(cz))[0][0])
+	#var going in
+	
+	if pivot_col in range(nr_decision_vars):
+		var_in = 'x'
+	elif pivot_col in range(nr_decision_vars, nr_decision_vars+nr_const):
+		var_in = 's'
+	else:
+		var_in = 'a'
 	temp = []
 	for i in range(num_rows):
 		if big_matrix[i][pivot_col] > 0:
@@ -85,10 +94,18 @@ while (True):
 		else: 
 			temp.append(9999)
 	pivot_row = int(np.where(temp == min(temp))[0][0])
-
+	#var going out
+	if Cb[pivot_row] == -M:
+		var_out = 'a'
+	elif Cb[pivot_row] != 0:
+		var_out = 'x'
+	else:
+		var_out = 's'
 	#gauss elim
 	pivot = big_matrix[pivot_row][pivot_col]
-
+	print(f'{var_in}_{pivot_col%3+1} going in {var_out}_{pivot_row%3+1} going out.')
+	_in.append(f'{var_in}_{pivot_col%3+1}')
+	out.append(f'{var_out}_{pivot_row%3+1}')
 	Cb[pivot_row] = obj[pivot_col]
 	big_matrix[pivot_row, :] = big_matrix[pivot_row, :]/pivot
 
@@ -107,9 +124,7 @@ while (True):
 
 	
 	if (all(i <= 0 for i in cz) or all(i !=0 for i in Cb)):
-		print(big_matrix)
-		print(cz)
 		print(f'finished in {iter+1} iterations. max value is = {z[-1]}')	
-		print(Cb)
+		print(f'final values : {big_matrix[:,-1]}, in: {_in}, out: {out}.')
 		break
 	iter += 1
